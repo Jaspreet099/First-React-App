2ded8f8d68f4dee7d42351b11be841e2d0237a12
Jaspreet singh <jaspreetsinghsaini099@gmail.com>
2022-06-14 00:01:36 +0530

    Counter created using react useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>lastName: {props.lName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
+
+// const Person = (props) => {
+//   return (
+//     <>
+//     <h1>Name: {props.name}</h1>
+//     <h2>lastName: {props.lName}</h2>
+//     <h2>Age: {props.age}</h2>
+//     </>
+//   )
+// }
 
 const App = () => {
+
+  const [counter, setCounter] = useState(0);
   // const name = "Jaspreet Singh";
   // const isNameShowing = false;
   return (
     // <div className="App">
     //   <h1> Hello,{name}</h1>
     // </div>
+    // <div className="App">
+    //   <Person name='Jaspreet' lName='Saini' age="19" />
+    //   <Person name='Sneha' lName='Farkya' age="19" />
+      
+    // </div>
     <div className="App">
-      <Person name='Jaspreet' lName='Saini' age="19" />
-      <Person name='Sneha' lName='Farkya' age="19" />
+     <button onClick={() => setCounter( (prevCount) => prevCount - 1)}>-</button>
+     <h1>{counter}</h1>
+     <button onClick={() => setCounter( (prevCount) => prevCount + 1)} >+</button>
       
     </div>
   );
